f7c3b8ec6ec2d1cd945d40ed24436cc222f2e27d
Simon Cochrane <simonjc999@gmail.com>
2023-12-04 16:49:26 +1100

    Code review version

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"sync"
+	"time"
 
 	"github.com/nearmap/nearmap-go-test/datasource"
 )
@@ -24,13 +26,23 @@ func main() {
 
 	dataSource := datasource.NewLocalDataSource(db, cache)
 
+	var wg sync.WaitGroup
 	for i := 0; i < 10; i++ {
+		wg.Add(1)
 		go func() {
+			defer wg.Done()
 			for j := 0; j < 50; j++ {
 				key := fmt.Sprintf("key%d", rand.Intn(10))
+				start := time.Now()
 				value, err := dataSource.Value(key)
-				// TODO:
+				if err != nil {
+					fmt.Printf("Received error")
+				} else {
+					fmt.Println("Request", key, "response", value.(string), "time:", time.Since(start).Milliseconds(), "ms")
+				}
 			}
 		}()
 	}
+
+	wg.Wait()
 }
